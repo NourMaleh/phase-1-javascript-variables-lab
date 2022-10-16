5fe65ed6be411f9b16d7add9dbd59dca1e583e6e
nour al maleh <nourmale7@gmail.com>
2022-10-16 14:55:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+companyName == 'Scuber'
+const companyName = 'Scuber';
+
+mostProfitableNeighborhood = "Chelsea"
+let mostProfitableNeighborhood = "Chelsea";
+
+companyCeo = "Susan Smith"
+let companyCeo = "Susan Smith";
